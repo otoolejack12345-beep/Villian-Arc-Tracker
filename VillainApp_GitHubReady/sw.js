3c017d0a275bdc07a4801117d80e6393d07d8fc5
otoolejack12345-beep <otoolejack12345@gmail.com>
2025-10-28 20:24:55 -0400

    Add files via upload

---
+++ VillainApp_GitHubReady/sw.js
@@ -0,0 +1,4 @@
+const cacheName = 'villain-arc-cache-v1';
+const assetsToCache = ['./index.html','./manifest.json','./icon.png','https://cdn.jsdelivr.net/npm/chart.js'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assetsToCache))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(response => response || fetch(e.request))); });
